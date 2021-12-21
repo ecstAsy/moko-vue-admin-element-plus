e5a5405edcebb8e1f43d0b45701fff7058a0cfe9
ecstAsy <sky_hmn@163.com>
2021-12-21 11:30:25 +0800

    add mock

---
+++ dist/assets/index.c0dc2055.js
@@ -0,0 +1,1 @@
+import{a4 as r,c as o,w as t,o as a,i as c}from"./vendor.25d6d6b7.js";/* empty css             */import{_ as s}from"./index.502d1c3e.js";const n={},d=c(" query ");function _(f,i){const e=r;return a(),o(e,null,{default:t(()=>[d]),_:1})}var x=s(n,[["render",_]]);export{x as default};
