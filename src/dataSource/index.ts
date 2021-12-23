c866ff33700ffa4c2136fcfbff470f6e7b5ff246
ecstAsy <sky_hmn@163.com>
2021-12-23 15:29:25 +0800

    v-infinite-scroll

--- src/dataSource/index.ts
+++ src/dataSource/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: ecstAsy
  * @Date: 2021-12-21 09:35:16
- * @LastEditTime: 2021-12-21 17:42:04
+ * @LastEditTime: 2021-12-23 15:12:08
  * @LastEditors: ecstAsy
  */
 import Mock from "mockjs";
@@ -33,26 +33,46 @@ const STATUS = [
   },
 ];
 
+const Tags = ["近期热门", "读者推荐", "设计语言", "人间常态", "隔岸观火", "釜底抽薪"];
+
+const getRandmArry = (arry:Array<string>, num:number) => {
+  let vs:Array<string> = [];
+  let i = 1;
+  while (i <= num) {
+    vs = [...vs, arry[Math.floor(Math.random() * arry.length)]];
+    i += 1;
+  }
+  return vs;
+};
+
+const Texts = [
+  "不要眼眶一红，就觉得人间不值得，散伙是人间常态，你我，又有什么例外！",
+  "夏虫不可语于冰，井蛙不可语于海。",
+  "生而未养，断指可还。生而养之，断头可还。未生而养，百世难还！",
+  "二十年芦叶满汀洲，寒沙带浅流。",
+  "柳下系船犹未稳，能几日，又中秋。",
+  "重过南楼。黄鹤断矶头，故人曾到否？旧江山浑是新愁。",
+  "欲买桂花同载酒，终不似，少年游。",
+];
+
 const data = () => Mock.mock({
   "list|10": [
     {
       id: "@increment",
       "avatar|+1": "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
       "name|+1": "@name",
-      date: "@date",
+      "cname|+1": "@cname",
+      date: "@datetime",
       "dynamicType|+1": ["近期热门", "读者推荐"],
       star: "@integer(60, 1000)",
       address: "@title(2)",
       "times|1-10": 10,
       "status|+1": Array.from(STATUS, ({ value }) => value),
-      // statusName():string {
-      //   let str = "";
-      //   const obj = STATUS.find((item) => item.value === this.status);
-      //   if (obj) {
-      //     str = obj.label;
-      //   }
-      //   return str;
-      // },
+      tags: new Set([...getRandmArry(Tags, 4)]),
+      like: "@integer(60, 1000)",
+      view: "@integer(60, 1000)",
+      article: "@cparagraph(10,20)",
+      "signature|+1": Texts,
     },
   ],
 });
--- src/views/list/query/index.vue
+++ src/views/list/query/index.vue
@@ -1,57 +1,63 @@
 <!--
  * @Author: ecstAsy
  * @Date: 2021-12-09 14:54:27
- * @LastEditTime: 2021-12-22 18:03:40
+ * @LastEditTime: 2021-12-23 15:27:28
  * @LastEditors: ecstAsy
 -->
 
 <template>
-  <el-card>
+  <el-card v-loading="loading">
     <ul
       v-infinite-scroll="load"
       class="infinite-list"
       style="overflow: auto"
     >
       <li
-        v-for="i in count"
+        v-for="(item,i) in dataArry"
         :key="i"
         class="infinite-list-item"
       >
         <el-row>
-          <el-col>
-            <b>Moko</b>
+          <el-col class="list-title">
+            <b>{{ item.name }}</b>
+            <div>{{ item.signature }}</div>
           </el-col>
-          <el-col>
-            <el-tag size="medium">
-              设计语言
-            </el-tag>
-            <el-tag size="medium">
-              设计语言
-            </el-tag>
-            <el-tag size="medium">
-              设计语言
-            </el-tag>
+          <el-col class="list-tags">
+            <template
+              v-for="tag in item.tags"
+              :key="tag"
+            >
+              <el-tag size="mini">
+                {{ tag }}
+              </el-tag>
+            </template>
           </el-col>
-          <el-col>
-            不要眼眶一红，就觉得人间不值得，散伙是人间常态，你我，又有什么例外！
-            夏虫不可语于冰，井蛙不可语于海。
-            生而未养，断指可还。生而养之，断头可还。未生而养，百世难还！
-            唐多令
-            宋 · 刘过
-            芦叶满汀洲，寒沙带浅流。
-            二十年重过南楼。
-            柳下系船犹未稳，能几日，又中秋。
-            黄鹤断矶头，故人曾到否？旧江山浑是新愁。
-            欲买桂花同载酒，终不似，少年游。
+          <el-col class="list-content">
+            {{ item.article }}
           </el-col>
-          <el-col>
-            <el-avatar
-              :size="20"
-              src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
-            />
+          <el-col class="list-info">
+            <moko-icon icon="Avatar" />
+            <el-button
+              type="text"
+              size="mini"
+            >
+              {{ item.cname }}
+            </el-button>
+            <span>
+              发布在
+              <a href="/">
+                {{ item.dynamicType }}
+              </a>
+            </span>
+            <span>{{ item.date }}</span>
           </el-col>
-          <el-col>
+          <el-col class="list-action">
+            <moko-icon icon="Star" />
+            <span>{{ item.star }}</span>
             <moko-icon icon="ChatDotRound" />
+            <span>{{ item.like }}</span>
+            <moko-icon icon="View" />
+            <span>{{ item.view }}</span>
           </el-col>
         </el-row>
       </li>
@@ -61,14 +67,21 @@
 
 <script lang="ts" setup>
 import { ref } from "vue";
+import { mockLists } from "@/dataSource";
 
-const count = ref(0);
-const load = () => {
-  count.value += 2;
+const dataArry = ref<Array<any>>([]);
+const loading = ref<boolean>(false);
+const load = async () => {
+  loading.value = true;
+  const data:any = await mockLists(1);
+  dataArry.value = [...dataArry.value, ...data.data.list];
+  loading.value = false;
 };
 </script>
 
 <style lang="scss">
+@import "@/styles/index.scss";
+
 .infinite-list {
   height: 88vh;
   padding: 0;
@@ -81,13 +94,61 @@ const load = () => {
 
   .infinite-list-item {
     display: flex;
-    background: var(--el-color-primary-light-9);
-    opacity: .8;
-    margin-bottom: 10px;
-    padding: 10px;
+    border-bottom: 1px dashed var(--el-color-primary-light-9);
+    padding: 10px 0;
     border-radius: 8px;
     & + .list-item {
       margin-top: 10px;
+    }
+
+    .list-title {
+      @include flex-row();
+      div {
+        width: 50%;
+        font-size: 12px;
+        height: 70px;
+        line-height: 18px;
+        letter-spacing: 2px;
+        writing-mode: vertical-rl;
+        background: url(@/assets/txt-bg.png) no-repeat;
+        background-size: 100% 100px;
+        background-origin: border-box;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+      }
+    }
+
+    .list-tags {
+      padding: 16px 0;
+      .el-tag {
+        margin-right: 16px;
+      }
+    }
+
+    .list-content {
+      font-size: 13px;
+      line-height: 26px;
+      background: url(@/assets/txt-bg.png) no-repeat;
+      background-origin: border-box;
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+    }
+
+    .list-info {
+      padding: 10px 0;
+      @include flex-row(flex-start);
+      @include f-s-c(13px, $font-color-regular);
+      span {
+        margin-left: 16px;
+      }
+    }
+
+    .list-action {
+      @include flex-row(flex-start);
+      span {
+        @include f-s-c(13px, $font-color-regular);
+        margin-right: 8px;
+      }
     }
   }
 }
