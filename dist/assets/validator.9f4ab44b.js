f3f0ee03252b2db9450bd1e985cfbdb44a9bd27f
hexiaoqiang <sky_hmn@163.com>
2022-10-21 17:02:53 +0800

    update:更新样板

--- dist/assets/validator.9f4ab44b.js
+++
@@ -1,1 +0,0 @@
-import{bt as i}from"./index.1b35f382.js";const n=o=>["",...i].includes(o);export{n as i};
---
+++ dist/assets/validator.bcec9554.js
@@ -0,0 +1,1 @@
+import{bt as i}from"./index.75d71485.js";const n=o=>["",...i].includes(o);export{n as i};
